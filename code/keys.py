rntz/knausj_talon

Rename {symbol,arrow,number,modifier,function,special}_key lists/captures

code/keys.py
@@ -16,12 +16,12 @@ default_f_digits = "one two three four five six seven eight nine ten eleven twel
 
 mod = Module()
 mod.list("letter", desc="The spoken phonetic alphabet")
-mod.list("symbol", desc="All symbols from the keyboard")
-mod.list("arrow", desc="All arrow keys")
-mod.list("number", desc="All number keys")
-mod.list("modifier", desc="All modifier keys")
-mod.list("function", desc="All function keys")
-mod.list("special", desc="All special keys")
+mod.list("symbol_key", desc="All symbols from the keyboard")
+mod.list("arrow_key", desc="All arrow keys")
+mod.list("number_key", desc="All number keys")
+mod.list("modifier_key", desc="All modifier keys")
+mod.list("function_key", desc="All function keys")
+mod.list("special_key", desc="All special keys")
 
 
 @mod.capture
@@ -30,17 +30,17 @@ def modifiers(m) -> str:
 
 
 @mod.capture
-def arrow(m) -> str:
+def arrow_key(m) -> str:
     "One directional arrow key"
 
 
 @mod.capture
-def arrows(m) -> str:
-    "One or more arrows separate by a space"
+def arrow_keys(m) -> str:
+    "One or more arrow keys separated by a space"
 
 
 @mod.capture
-def number(m) -> str:
+def number_key(m) -> str:
     "One number key"
 
 
@@ -55,23 +55,23 @@ def letters(m) -> list:
 
 
 @mod.capture
-def symbol(m) -> str:
+def symbol_key(m) -> str:
     "One symbol key"
 
 
 @mod.capture
-def function(m) -> str:
+def function_key(m) -> str:
     "One function key"
 
 
 @mod.capture
-def special(m) -> str:
+def special_key(m) -> str:
     "One special key"
 
 
 @mod.capture
-def any(m) -> str:
-    "Any one key"
+def unmodified_key(m) -> str:
+    "A single key with no modifiers"
 
 
 @mod.capture
@@ -79,8 +79,13 @@ def key(m) -> str:
     "A single key with optional modifiers"
 
 
+@mod.capture
+def keys(m) -> str:
+    "A sequence of one or more keys with optional modifiers"
+
+
 ctx = Context()
-ctx.lists["self.modifier"] = {
+ctx.lists["self.modifier_key"] = {
     "alt": "alt",
     "command": "cmd",
     "control": "ctrl",  #'troll':   'ctrl',
@@ -90,7 +95,7 @@ ctx.lists["self.modifier"] = {
 }
 alphabet = dict(zip(default_alphabet, letters_string))
 ctx.lists["self.letter"] = alphabet
-ctx.lists["self.symbol"] = {
+ctx.lists["self.symbol_key"] = {
     "back tick": "`",
     "`": "`",
     "comma": ",",
@@ -156,8 +161,8 @@ ctx.lists["self.symbol"] = {
 }
 
 
-ctx.lists["self.number"] = dict(zip(default_digits, numbers))
-ctx.lists["self.arrow"] = {
+ctx.lists["self.number_key"] = dict(zip(default_digits, numbers))
+ctx.lists["self.arrow_key"] = {
     "down": "down",
     "left": "left",
     "right": "right",
@@ -183,30 +188,30 @@ alternate_keys = {
 }
 keys = {k: k for k in simple_keys}
 keys.update(alternate_keys)
-ctx.lists["self.special"] = keys
-ctx.lists["self.function"] = {
+ctx.lists["self.special_key"] = keys
+ctx.lists["self.function_key"] = {
     f"F {default_f_digits[i]}": f"f{i + 1}" for i in range(12)
 }
 
 
-@ctx.capture(rule="{self.modifier}+")
+@ctx.capture(rule="{self.modifier_key}+")
 def modifiers(m):
-    return "-".join(m.modifier_list)
+    return "-".join(m.modifier_key_list)
 
 
-@ctx.capture(rule="{self.arrow}")
-def arrow(m) -> str:
-    return m.arrow
+@ctx.capture(rule="{self.arrow_key}")
+def arrow_key(m) -> str:
+    return m.arrow_key
 
 
-@ctx.capture(rule="<self.arrow>+")
-def arrows(m) -> str:
+@ctx.capture(rule="<self.arrow_key>+")
+def arrow_keys(m) -> str:
     return str(m)
 
 
-@ctx.capture(rule="{self.number}")
-def number(m):
-    return m.number
+@ctx.capture(rule="{self.number_key}")
+def number_key(m):
+    return m.number_key
 
 
 @ctx.capture(rule="{self.letter}")
@@ -214,32 +219,41 @@ def letter(m):
     return m.letter
 
 
-@ctx.capture(rule="{self.special}")
-def special(m):
-    return m.special
+@ctx.capture(rule="{self.special_key}")
+def special_key(m):
+    return m.special_key
 
 
-@ctx.capture(rule="{self.symbol}")
-def symbol(m):
-    return m.symbol
+@ctx.capture(rule="{self.symbol_key}")
+def symbol_key(m):
+    return m.symbol_key
 
 
-@ctx.capture(rule="{self.function}")
-def function(m):
-    return m.function
+@ctx.capture(rule="{self.function_key}")
+def function_key(m):
+    return m.function_key
 
 
 @ctx.capture(
-    rule="(<self.arrow> | <self.number> | <self.letter> | <self.symbol> | <self.function> | <self.special>)"
+    rule="( <self.letter> | <self.number_key> | <self.symbol_key> "
+         "| <self.arrow_key> | <self.function_key> | <self.special_key> )"
 )
-def any(m) -> str:
+def unmodified_key(m) -> str:
     return str(m)
 
 
-@ctx.capture(rule="<self.modifiers> <self.any>")
+@ctx.capture(rule="{self.modifier}* <self.unmodified_key>")
 def key(m) -> str:
-    mods = m.modifiers
-    return "-".join([mods] + [m.any])
+    try:
+        mods = m.modifier_list
+    except AttributeError:
+        mods = []
+    return "-".join(mods + [m.unmodified_key])
+
+
+@ctx.capture(rule="<self.key>+")
+def keys(m) -> str:
+    return " ".join(m.key_list)
 
 
 @ctx.capture(rule="{self.letter}+")

eprev/talon

code/keys.py
@@ -16,12 +16,12 @@ default_f_digits = "one two three four five six seven eight nine ten eleven twel
 
 mod = Module()
 mod.list("letter", desc="The spoken phonetic alphabet")
-mod.list("symbol", desc="All symbols from the keyboard")
-mod.list("arrow", desc="All arrow keys")
-mod.list("number", desc="All number keys")
-mod.list("modifier", desc="All modifier keys")
-mod.list("function", desc="All function keys")
-mod.list("special", desc="All special keys")
+mod.list("symbol_key", desc="All symbols from the keyboard")
+mod.list("arrow_key", desc="All arrow keys")
+mod.list("number_key", desc="All number keys")
+mod.list("modifier_key", desc="All modifier keys")
+mod.list("function_key", desc="All function keys")
+mod.list("special_key", desc="All special keys")
 
 
 @mod.capture
@@ -30,17 +30,17 @@ def modifiers(m) -> str:
 
 
 @mod.capture
-def arrow(m) -> str:
+def arrow_key(m) -> str:
     "One directional arrow key"
 
 
 @mod.capture
-def arrows(m) -> str:
-    "One or more arrows separate by a space"
+def arrow_keys(m) -> str:
+    "One or more arrow keys separated by a space"
 
 
 @mod.capture
-def number(m) -> str:
+def number_key(m) -> str:
     "One number key"
 
 
@@ -55,23 +55,23 @@ def letters(m) -> list:
 
 
 @mod.capture
-def symbol(m) -> str:
+def symbol_key(m) -> str:
     "One symbol key"
 
 
 @mod.capture
-def function(m) -> str:
+def function_key(m) -> str:
     "One function key"
 
 
 @mod.capture
-def special(m) -> str:
+def special_key(m) -> str:
     "One special key"
 
 
 @mod.capture
-def any(m) -> str:
-    "Any one key"
+def unmodified_key(m) -> str:
+    "A single key with no modifiers"
 
 
 @mod.capture
@@ -79,8 +79,13 @@ def key(m) -> str:
     "A single key with optional modifiers"
 
 
+@mod.capture
+def keys(m) -> str:
+    "A sequence of one or more keys with optional modifiers"
+
+
 ctx = Context()
-ctx.lists["self.modifier"] = {
+ctx.lists["self.modifier_key"] = {
     "alt": "alt",
     "command": "cmd",
     "control": "ctrl",  #'troll':   'ctrl',
@@ -90,7 +95,7 @@ ctx.lists["self.modifier"] = {
 }
 alphabet = dict(zip(default_alphabet, letters_string))
 ctx.lists["self.letter"] = alphabet
-ctx.lists["self.symbol"] = {
+ctx.lists["self.symbol_key"] = {
     "back tick": "`",
     "`": "`",
     "comma": ",",
@@ -156,8 +161,8 @@ ctx.lists["self.symbol"] = {
 }
 
 
-ctx.lists["self.number"] = dict(zip(default_digits, numbers))
-ctx.lists["self.arrow"] = {
+ctx.lists["self.number_key"] = dict(zip(default_digits, numbers))
+ctx.lists["self.arrow_key"] = {
     "down": "down",
     "left": "left",
     "right": "right",
@@ -183,30 +188,30 @@ alternate_keys = {
 }
 keys = {k: k for k in simple_keys}
 keys.update(alternate_keys)
-ctx.lists["self.special"] = keys
-ctx.lists["self.function"] = {
+ctx.lists["self.special_key"] = keys
+ctx.lists["self.function_key"] = {
     f"F {default_f_digits[i]}": f"f{i + 1}" for i in range(12)
 }
 
 
-@ctx.capture(rule="{self.modifier}+")
+@ctx.capture(rule="{self.modifier_key}+")
 def modifiers(m):
-    return "-".join(m.modifier_list)
+    return "-".join(m.modifier_key_list)
 
 
-@ctx.capture(rule="{self.arrow}")
-def arrow(m) -> str:
-    return m.arrow
+@ctx.capture(rule="{self.arrow_key}")
+def arrow_key(m) -> str:
+    return m.arrow_key
 
 
-@ctx.capture(rule="<self.arrow>+")
-def arrows(m) -> str:
+@ctx.capture(rule="<self.arrow_key>+")
+def arrow_keys(m) -> str:
     return str(m)
 
 
-@ctx.capture(rule="{self.number}")
-def number(m):
-    return m.number
+@ctx.capture(rule="{self.number_key}")
+def number_key(m):
+    return m.number_key
 
 
 @ctx.capture(rule="{self.letter}")
@@ -214,32 +219,41 @@ def letter(m):
     return m.letter
 
 
-@ctx.capture(rule="{self.special}")
-def special(m):
-    return m.special
+@ctx.capture(rule="{self.special_key}")
+def special_key(m):
+    return m.special_key
 
 
-@ctx.capture(rule="{self.symbol}")
-def symbol(m):
-    return m.symbol
+@ctx.capture(rule="{self.symbol_key}")
+def symbol_key(m):
+    return m.symbol_key
 
 
-@ctx.capture(rule="{self.function}")
-def function(m):
-    return m.function
+@ctx.capture(rule="{self.function_key}")
+def function_key(m):
+    return m.function_key
 
 
 @ctx.capture(
-    rule="(<self.arrow> | <self.number> | <self.letter> | <self.symbol> | <self.function> | <self.special>)"
+    rule="( <self.letter> | <self.number_key> | <self.symbol_key> "
+         "| <self.arrow_key> | <self.function_key> | <self.special_key> )"
 )
-def any(m) -> str:
+def unmodified_key(m) -> str:
     return str(m)
 
 
-@ctx.capture(rule="<self.modifiers> <self.any>")
+@ctx.capture(rule="{self.modifier_key}* <self.unmodified_key>")
 def key(m) -> str:
-    mods = m.modifiers
-    return "-".join([mods] + [m.any])
+    try:
+        mods = m.modifier_key_list
+    except AttributeError:
+        mods = []
+    return "-".join(mods + [m.unmodified_key])
+
+
+@ctx.capture(rule="<self.key>+")
+def keys(m) -> str:
+    return " ".join(m.key_list)
 
 
 @ctx.capture(rule="{self.letter}+")